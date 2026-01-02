2dd497e1cf7aadd50104b6890acb9d14506373e0
Mateo <167565464+matey-0@users.noreply.github.com>
2026-01-02 16:35:31 -0500

    Update __ufp_extras.fish

--- functions/__ufp_extras.fish
+++ functions/__ufp_extras.fish
@@ -12,8 +12,7 @@ function __ufp_extras
         for box in (distrobox list | awk 'NR>1 {print $3}')
             set_color purple; echo "Upgrading $box distrobox"; set_color normal
             distrobox-enter -n $box -- fish -c "ufp -u"
-        end
-        set_color purple; echo "Finished upgrading all distroboxes found"; set_color normal
+        end && set_color purple; echo "Finished upgrading all distroboxes found"; set_color normal
         if command -v flatpak > /dev/null
             command sudo flatpak upgrade -y && flatpak upgrade -y
         end
