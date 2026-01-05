e400e28c92381c6617488d3a5d9bd52dcab5f40f
Mateo <167565464+matey-0@users.noreply.github.com>
2026-01-05 12:21:34 -0500

    Update __ufp_extras.fish

--- functions/__ufp_extras.fish
+++ functions/__ufp_extras.fish
@@ -1,31 +1,36 @@
 function __ufp_extras
+    set main_upgrade_status $status
 
-    set main_upgrade_status $status
-    if command -v sbctl > /dev/null
-        set_color purple; echo "Checking secureboot signatures"; set_color normal
-        command sudo sbctl sign-all 2> /dev/null
-        # command sudo sbctl sign -s /boot/vmlinuz-*-cachyos*.lto.* 2> /dev/null
+    if not __is_container
+        if command -v sbctl >/dev/null
+            set_color purple; echo "Checking secureboot signatures"; set_color normal
+            command sudo sbctl sign-all 2>/dev/null
+        end
+
+        if command -v distrobox >/dev/null
+            for box in (distrobox list | awk 'NR>1 {print $3}')
+                set_color purple; echo "Upgrading $box distrobox"; set_color normal
+                distrobox-enter -n $box -- fish -c "ufp -u"
+            end; set_color purple; echo "Finished upgrading all distroboxes found"; set_color normal
+        end
     end
 
-    if command -v distrobox > /dev/null
-        # distrobox-upgrade --all
-        for box in (distrobox list | awk 'NR>1 {print $3}')
-            set_color purple; echo "Upgrading $box distrobox"; set_color normal
-            distrobox-enter -n $box -- fish -c "ufp -u"
-        end && set_color purple; echo "Finished upgrading all distroboxes found"; set_color normal
-        if command -v flatpak > /dev/null
+    if not __is_container
+        if command -v flatpak >/dev/null
             set_color purple; echo "Upgrading system flatpaks"; set_color normal
             command sudo flatpak upgrade -y
             set_color purple; echo "Upgrading user flatpaks"; set_color normal
             flatpak upgrade -y
         end
-        if command -v hblock > /dev/null
+
+        if command -v hblock >/dev/null
             set_color purple; echo "Upgrading hBlock filterlists"; set_color normal
             command sudo hblock -S ~/.config/hblock/deny.list
         end
-        if functions -q fisher 
+
+        if functions -q fisher
             set_color purple; echo "Updating fisher plugins"; set_color normal
             fisher update
         end
-    end
+    end
 end
