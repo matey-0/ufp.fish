d9c0633a028457057563c29214858b1877cb3b73
Mateo <167565464+matey-0@users.noreply.github.com>
2026-02-11 19:42:47 -0500

    Refactor __ufp_repos function for improved path handling

--- functions/__ufp_repos.fish
+++ functions/__ufp_repos.fish
@@ -1,23 +1,27 @@
 function __ufp_repos
-    if not set -q REPO_PATH
-        echo "It looks like you haven't set your repository directory yet."
-        read -p 'echo "Enter the full path to your repos (e.g. ~/Development/Repos): "' input_path
+    set -l default_path "~/Development/Repos"
+
+    if not set -q REPO_PATH; or not test -d "$REPO_PATH"
+        echo "It looks like your repository directory isn't set or is invalid."
         
-        set -U REPO_PATH (eval echo $input_path)
+        read -P "Enter the path to your repos [$default_path]: " -D "$default_path" input_path
+
+        set -l expanded_path (eval echo $input_path)
+        set -U REPO_PATH $expanded_path
         
-        echo "Your repos are set as being here: $REPO_PATH"
-        echo "You can manually edit this later in ~/.config/fish/fish_variables"
-        echo ""
+        echo "Path set to: $REPO_PATH"
     end
 
-    for d in $REPO_PATH/*/
-        if test -d $d/.git
-            set_color blue; echo "Checking "(basename $d)"..." ; set_color normal
+    set -l target_path (string trim --right --chars=/ $REPO_PATH)
 
-            if not git -C $d diff-index --quiet HEAD --
+    for d in $target_path/*
+        if test -d "$d/.git"
+            set_color blue; echo "Checking "(basename "$d")"..." ; set_color normal
+
+            if not git -C "$d" diff-index --quiet HEAD --
                 set_color yellow; echo "Uncommitted changes found. Skipping pull."; set_color normal
             else
-                git -C $d pull
+                git -C "$d" pull
             end
             echo ""
         end
