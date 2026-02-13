4f8c30fac52898e5ba5929da1e4762d1e4f7db9a
Mateo <167565464+matey-0@users.noreply.github.com>
2026-02-13 16:18:25 -0500

    Update __ufp_repos.fish

--- functions/__ufp_repos.fish
+++ functions/__ufp_repos.fish
@@ -1,30 +1,42 @@
 function __ufp_repos
-    set -l default_path "~/Development/Repos"
+    set -l default_path "$HOME/Development/Repos"
 
     if not set -q REPO_PATH; or not test -d "$REPO_PATH"
-        echo "It looks like your repository directory isn't set or is invalid."
+        echo "It looks like your repository directory isn't set, or is invalid."
         
-        read -P "Enter the path to your repos [$default_path]: " -D "$default_path" input_path
+        read -P "Enter the path to your repos [$default_path]: " input_path
 
-        set -l expanded_path (eval echo $input_path)
-        set -U REPO_PATH $expanded_path
-        
-        echo "Path set to: $REPO_PATH"
+        if test -z "$input_path"
+            set input_path $default_path
+        end
+
+        set -l expanded_path (string replace -r '^~' $HOME $input_path)
+
+        if test -d "$expanded_path"
+            set -U REPO_PATH $expanded_path
+            echo "Path set to: $REPO_PATH"
+        else
+            set_color red; echo "Error: Directory '$expanded_path' does not exist."; set_color normal
+            return 1
+        end
     end
 
     set -l target_path (string trim --right --chars=/ $REPO_PATH)
 
-    set_color purple; echo "Updating repos"; set_color normal
+    set_color purple; echo "Updating repos in $target_path"; set_color normal
 
     for d in $target_path/*
         if test -d "$d/.git"
-
             set_color cyan; echo "Checking "(basename "$d")"..." ; set_color normal
 
-            if not git -C "$d" diff-index --quiet HEAD --
-                set_color yellow; echo "Uncommitted changes found. Skipping pull."; set_color normal
+            if test -n "(git -C "$d" status --porcelain)"
+                set_color yellow; echo "Uncommitted changes found; skipping pull."; set_color normal
             else
-                git -C "$d" pull
+                if git -C "$d" pull --ff-only > /dev/null 2>&1
+                    set_color green; echo "Updated successfully"; set_color normal
+                else
+                    set_color red; echo "Pull failed"; set_color normal
+                end
             end
             echo ""
         end
