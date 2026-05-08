35a53710dcf3c5ed3827205fc08404e25c5affd0
matey-0 <167565464+matey-0@users.noreply.github.com>
2026-05-08 14:20:19 -0400

    Delete functions/__ufp_fullupgrade.fish

--- functions/__ufp_fullupgrade.fish
+++
@@ -1,8 +0,0 @@
-function __ufp_fullupgrade
-    sudo -v
-    set -l os_env (__get_os_info)
-    set -l main_upgrade_status 0
-    __ufp_upgrade
-    __ufp_extras
-    return $main_upgrade_status
-end
